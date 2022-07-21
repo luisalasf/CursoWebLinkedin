d6a14399b1e10fff50ae1bc47a68d01742bec385
Luis Salas <luispiyu007@gmail.com>
2022-07-21 18:25:10 -0500

    Listeners mouse, keyboard, video

--- scripts/eventos.js
+++ scripts/eventos.js
@@ -1,5 +1,11 @@
 "use strict"
 
+/*          Evento load     */
+window.addEventListener("load", ()=> {
+    console.log("la ventana se ha cargado correctamente");
+});
+
+/*          eventos del mouse           */
 const boton = document.getElementById("boton");
 
 boton.addEventListener('click', function xd () {
@@ -13,4 +19,40 @@ boton.addEventListener("mouseover", () => {
 boton.addEventListener("mouseout", ()=>{
     console.log("El mouse salió del botón")
 })
+
+/*              eventos del teclado             */
+//  cuando recién se presiona una tecla
+window.addEventListener("keydown", (event) => {
+    console.log("Se está pulsando una tecla");
+    console.log(event["key"]);
+})
+//  Cuando se deja presionada la tecla
+window.addEventListener("keypress", (event) => {
+    console.log("Tecla pulsada");
+    console.log(event["key"]);
+})
+//cuando la tecla se deja de presionar
+
+window.addEventListener("keypress", (event) => {
+    console.log("esta tecla ya no se está presionando");
+    console.log(event["key"]);
+})
+
+/*              Eventos con reproductores de video          */
+const video = document.getElementById("video");
+//  Botón play del reproductor de video
+video.addEventListener("play", () => {
+    console.log("Se pulsó el botón play del video");
+});
+
+//  Pulsa botón pause del video
+video.addEventListener("pause", () => {
+    console.log("Se pulsó el botón pause del video");
+});
+
+//  Moviendo de posicion el video
+video.addEventListener("seeking", function () {
+    console.log("Se está buscando en el video: " + this.currentTime);
+});
+
 
