f44d0ad9fe82d167292276daab7d7065724b480c
Luis Salas <luispiyu007@gmail.com>
2022-07-28 20:14:01 -0500

    Arrays, filters

--- scripts/arrys.js
+++ scripts/arrys.js
@@ -10,3 +10,30 @@ parrafos = Array.from(parrafos);
 parrafos = parrafos.map(parrafo => parrafo.textContent)
 
 console.log(parrafos);
+
+for (let key in parrafos) {
+    console.log(key, parrafos[key], typeof key);
+}
+
+parrafos.forEach((element, i) => {
+    console.log(i, element, typeof i);
+});
+
+
+var menu = [
+    {name: "Pizza", price: 90, country: "Italy"},   
+    {name: "Taco", price: 20, country: "Mexico"},
+    {name: "Sushi", price: 80, country: "Japon"},
+    {name: "Quesadilla", price: 50, country: "Mexico"}
+] 
+
+console.log(menu.find(platillo => platillo.price > 50));
+
+console.log(menu.findIndex((platillo) => platillo.name == "Taco"));
+console.log(menu.findIndex((platillo) => platillo.name == "Sushi"));
+
+console.log(menu.filter(platillo => platillo.country == "Mexico"));
+console.log(menu.filter(platillo => platillo.price  > 20));
+
+console.log(menu.some(platillo => platillo.price <= 50)); //hay algun platillo menor o igual a 50 peso?
+console.log(menu.every(platillo => platillo.price >= 20));//todos los platillos son mayores a 20 peso?
